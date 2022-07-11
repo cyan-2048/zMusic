092bd763d95bf9c161a8b0f4290708a78456e511
Cyan <72785466+cyan-2048@users.noreply.github.com>
2022-07-11 20:59:22 +0800

    this routing thing is weird

--- src/routes/Album.svelte
+++ src/routes/Album.svelte
@@ -1,12 +1,14 @@
 <script>
 	import { onMount } from "svelte";
+	import { location } from "svelte-spa-router";
 
 	import { pop } from "svelte-spa-router/Router.svelte";
 	import { get } from "svelte/store";
 	import Tabs from "../components/Tabs.svelte";
+	import { last } from "../lib/helper";
 	import { albums, fadeScaleIn, sn, songs } from "../lib/shared";
 
-	export let params;
+	const hash = last($location.split("/"));
 
 	let selected = 0;
 	let tabs_el;
@@ -14,16 +16,15 @@
 
 	onMount(() => sn.focus());
 
-	console.log(params);
-
-	$: _songs = get(albums)
-		.get(params.hash === "null" ? null : params.hash)
-		.songs.map((a) => $songs.get(a))
-		.sort((a, b) => {
-			const _a = a.track || 0;
-			const _b = b.track || 0;
-			return _a - _b;
-		});
+	const _songs =
+		$albums
+			.get(hash === "null" ? null : hash)
+			?.songs.map((a) => $songs.get(a))
+			.sort((a, b) => {
+				const _a = a.track || 0;
+				const _b = b.track || 0;
+				return _a - _b;
+			}) || [];
 </script>
 
 <svelte:window
